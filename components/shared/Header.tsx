784c023c3c9db55854ee0aba1998fc16b084654f
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-10 23:16:40 +0800

    feat(components): Created the header
    This creation includes the desktop header and mobile header which includes mobile navigation
    
    BREAKING CHANGE: This header would require the sheet component from shadcn.ui and also most of the nav links are not routed yet

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+"use client";
+
+import { jost } from "@/lib/fonts";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import NavItems from "./NavItems";
+import { headerButtons } from "@/constants";
+import Image from "next/image";
+import Icon from "./Icon";
+import MobileNav from "./MobileNav";
+
+export const LogoLink = () => {
+  return (
+    <Link href="/">
+      <p className={`p-regular-24 ${jost.className}`}>LILI BEE JEWELS</p>
+    </Link>
+  );
+};
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [isMobile, setIsMobile] = useState<boolean>(false); // Provide a default value for isMobile
 
-export default Header
+  useEffect(() => {
+    setIsMobile(window.innerWidth < 620); // Fix the type error by providing a default value for the useState hook
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 620);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        {/* Logo */}
+        <LogoLink />
+
+        {/* Nav Items */}
+        <nav className="md:flex-between hidden w-full max-w-xs">
+          <NavItems />
+        </nav>
+
+        {/* Button Group */}
+        <div className="flex items-center gap-6">
+          {headerButtons
+            .filter((button) => !isMobile || button.mobile)
+            .map((button) => {
+              return (
+                <div key={button.route}>
+                  <Link href={button.route}>
+                    <Image
+                      src={button.icon}
+                      alt={button.label}
+                      width={24}
+                      height={24}
+                    />
+                  </Link>
+                </div>
+              );
+            })}
+            
+            {/* Mobile Nav Button */}
+            <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+import Icon from "./Icon";
+import NavItems from "./NavItems";
+
+const MobileNav = () => {
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Icon
+            path="/assets/icons/menu.svg"
+            alt="menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent
+          side={"top"}
+          className="flex flex-col gap-6 bg-white md:hidden max-h-[100vh] p-10 rounded-b-lg"
+        >
+          <NavItems fontStyle="p-medium-24" />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
+
+export default MobileNav;
