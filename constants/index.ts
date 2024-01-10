22a59991e0cb3194e7a8c238127218ced31d1962
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-10 23:12:05 +0800

    feat(constants): created a index file for all constants
    These constants will be used across the codebase

---
+++ constants/index.ts
@@ -0,0 +1,50 @@
+export const headerLinks = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "Store",
+    route: "/store",
+  },
+  {
+    label: "Wishlist",
+    route: "/wishlist",
+    mobile: true, 
+  },
+  {
+    label: "About Us",
+    route: "/about",
+  },
+  {
+    label: "Contact Us",
+    route: "/contact",
+  }
+];
+
+export const headerButtons = [
+  {
+    label: "search",
+    icon: "/assets/icons/search.svg",
+    route: "/search",
+    mobile: false,
+  },
+  {
+    label: "wishlist",
+    icon: "/assets/icons/heart.svg",
+    route: "/wishlist",
+    mobile: false,
+  },
+  {
+    label: "profile",
+    icon: "/assets/icons/user.svg",
+    route: "/profile",
+    mobile: true,
+  },
+  {
+    label: "shopping-bag",
+    icon: "/assets/icons/shopping-bag.svg",
+    route: "/shopping-bag",
+    mobile: true,
+  }
+];
