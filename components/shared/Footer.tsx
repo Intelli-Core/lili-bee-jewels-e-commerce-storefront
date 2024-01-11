600d393c27bcc38fb563632aba1cecad831be0a8
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-11 19:21:08 +0800

    refactor(components): added margin to the footer
    Added a margin top of 20px in the footer component

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -2,7 +2,7 @@ import { LogoLink } from "./Header";
 
 const Footer = () => {
   return (
-    <footer className="border-t">
+    <footer className="border-t mt-5">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <LogoLink />
         <p>© 2022 Lili Bee Jewels. All Rights reserved.</p>
