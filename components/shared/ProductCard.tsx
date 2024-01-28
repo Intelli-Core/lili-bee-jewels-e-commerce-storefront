fc643d85662fc07eedbfeb9739f239ed85cf39d8
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-28 15:13:34 +0800

    LBJEC-19 refactor(components): uodated ProductGrid, ProductCard, and, FilterCard

--- components/shared/ProductCard.tsx
+++ components/shared/ProductCard.tsx
@@ -1,26 +1,32 @@
-import React from "react";
 import { Card } from "@/components/ui/card";
-import Image from "next/image";
 import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
 
-const ProductCard = ({ name, description, price, image }: Product) => {
+type ProductCardProps = {
+  product: Product;
+};
+
+const ProductCard = ({ product }: ProductCardProps) => {
   return (
-    <div className="flex flex-col gap-2.5 md:gap-2.5">
-      <Card className="w-full max-w-sm min-w-[50%] md:min-w-[20%] rounded-[8px] bg-white">
-        <Image
-          src="/assets/images/placeholders/image-placeholder.svg"
-          alt="product-card"
-          width={350}
-          height={350}
-          className="object-contain object-center"
-          priority
-        />
-      </Card>
-      <div className="flex flex-col gap-1">
-        <p className="p-medium-12">{name}</p>
-        <p className="p-regular-14">${price}</p>
+    <Link href={`/store/products/${product.id}`}>
+      <div className="flex flex-col gap-2.5 md:gap-2.5">
+        <Card className="w-full max-w-sm min-w-[50%] md:min-w-[20%] rounded-[8px] bg-white">
+          <Image
+            src="/assets/images/placeholders/image-placeholder.svg"
+            alt="product-card"
+            width={350}
+            height={350}
+            className="object-contain object-center"
+            priority
+          />
+        </Card>
+        <div className="flex flex-col gap-1">
+          <p className="p-medium-12 truncate">{product.name}</p>
+          <p className="p-regular-14">${product.price}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
