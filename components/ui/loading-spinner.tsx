948566c30ecec93ce19c116eff133052f1ee640a
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-28 15:17:04 +0800

    LBJEC-19 feat(components): added a loading spinner

---
+++ components/ui/loading-spinner.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Spinner } from "@nextui-org/react";
+
+export default function SpinnerLoader() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Spinner />
+    </div>
+  );
+}
