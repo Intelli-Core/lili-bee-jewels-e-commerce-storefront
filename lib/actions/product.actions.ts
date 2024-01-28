a1621af4769f379efde4ef887dbc568ccfd4f793
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-28 15:20:37 +0800

    LBJEC-19 feat(lib/actions): added product actions for fetching products and filters

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,43 @@
+import { FilterProps, Filters, Product } from "@/types";
+
+async function getProducts(): Promise<Product[]> {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const url = `${process.env.DOMAIN}/product/`;
+
+  const response = await fetch(url, { cache: "no-store" });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function getProductsByFilter(filters: FilterProps): Promise<Product[]> {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const { category, material, size, sort } = filters;
+  const url = `${process.env.DOMAIN}/product/?category=${category}&material=${material}&size=${size}&sort=${sort}`;
+
+  const response = await fetch(url, { cache: "no-store" });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function getProductFilters(): Promise<Filters> {
+  const url = `${process.env.DOMAIN}/product/filters/`;
+  const res = await fetch(url, { cache: "no-store" });
+
+  return res.json();
+}
+
+export { getProducts, getProductsByFilter, getProductFilters };
