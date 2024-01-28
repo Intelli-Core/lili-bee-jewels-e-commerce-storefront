3e148fa5e9b5bed9ef55936875727a6d09975de2
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-01-28 15:09:23 +0800

    LBJEC-19 refactor(layout/app): made root layout to include Providers component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,23 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 import { jost } from "@/lib/fonts";
+import { Providers } from "@/components/shared/Providers";
 
 export const metadata: Metadata = {
-  title: 'Lili Bee Jewels',
-  description: 'Premimum jewelry for the affordable prices',
-}
+  title: "Lili Bee Jewels",
+  description: "Premimum jewelry for the affordable prices",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={jost.className}>{children}</body>
+      <body className={jost.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
