2f22565385f72e4aff72176c715a4d2dbd9e8a40
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-03-26 21:23:23 +0800

    LBJEC-17 feat(next.config): added image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d30vxck32ggp8b.cloudfront.net",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
