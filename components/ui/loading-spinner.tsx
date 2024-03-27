827f67fb0cf65d83989d17b26e4079bb80294025
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-03-27 16:09:45 +0800

    LBJEC-17 refactor(components/ui): updated loading-spinner

--- components/ui/loading-spinner.tsx
+++ components/ui/loading-spinner.tsx
@@ -1,10 +1,8 @@
-"use client";
-
 import { Spinner } from "@nextui-org/react";
 
 export default function SpinnerLoader() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center h-full">
       <Spinner />
     </div>
   );
